df55cf2e38b0c501b3397ba70e55c2fb893cf937
Azfa025 <azfaherm@gmail.com>
2025-11-18 11:18:24 +0700

    Game loop

--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
-def main(): pass
+import pygame #import pygame
+
+# inisialisasi pygame
+pygame.init()
+pygame.display.set_mode((400, 500))
+pygame.display.set_caption('Billiard Game')
+
+def main():
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+      
+            if event.type == pygame.QUIT:
+                running = False
 
 if __name__=='__main__': main()
